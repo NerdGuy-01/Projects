2839cd0062895e987c61785c5f25fbd0af1624d0
Dhruv-ver2 <dhruv2vaishnav@gmail.com>
2025-02-10 10:30:37 +0530

    Update encry_decry.c
    dhruv's update

--- C-Projects/encry_decry.c
+++ C-Projects/encry_decry.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
 #include<string.h>
 
-#define WORDS 100
-int main(){
- char words[WORDS];
-  printf("Enter Passoword");
- fflush(stdout);
-  fgets(words,sizeof(words),stdin);
-  return 0;
+void main(){
+    char string[100];
+    printf("Enter the string:");
+    scanf("%s",string);
+
+    for(int i=0;string[i]!='\0';i++){
+        if((string[i]>=65 && string[i]<=90) || (string[i]>=97 && string[i]<=122))
+            string[i]+=2;
+    
+    }
+
+    printf("The encrypted string is:- %s",string);
 }
+
+// #define WORDS 100
+// int main(){
+//  char words[WORDS];
+//   printf("Enter Passoword");
+//  fflush(stdout);
+//   fgets(words,sizeof(words),stdin);
+//   return 0;
+// }
