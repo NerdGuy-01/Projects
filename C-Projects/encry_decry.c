727ae015b69b2915486e416bb134249effe64c5a
Aarish mansur <aarishmansur@gmail.com>
2025-02-10 10:19:49 +0530

    Update encry_decry.c
    Input from user

--- C-Projects/encry_decry.c
+++ C-Projects/encry_decry.c
@@ -1,1 +1,10 @@
-done
+#include<stdio.h>
+#include<string.h>
+
+#define WORDS 100
+int main(){
+ char words[WORDS];
+  printf("Enter Passoword");
+  fgets(words,sizeof(words),stdin);
+  return 0;
+}
