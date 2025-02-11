96c799830131aa09de2d1835f29d813c366a6919
Aarish mansur <aarishmansur@gmail.com>
2025-02-11 22:15:03 +0530

    Update encry_decry.c
    Just add decryption option too, I know its easy but still

--- C-Projects/encry_decry.c
+++ C-Projects/encry_decry.c
@@ -13,6 +13,16 @@ void main(){
     }
 
     printf("The encrypted string is:- %s",string);
+  // For decryption
+    printf("\nWant to decrypt?(yes or no )");
+scanf("%s",choice);
+if(strcmp(choice,"yes")==0 || strcmp(choice,"Yes")==0){
+    for(int i=0;string[i]!='\0';i++){
+        string[i] -= 2;
+    }
+    printf("Your original passoword is : %s",string);
+}
+}
 }
 
 // #define WORDS 100
