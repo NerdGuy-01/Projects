9859e1c3822982cae82ed1f25a22160453f60266
Dhruv-ver2 <dhruv2vaishnav@gmail.com>
2025-02-12 22:41:59 +0530

    Update encry_decry.c
    added decryption functions

--- C-Projects/encry_decry.c
+++ C-Projects/encry_decry.c
@@ -24,6 +24,29 @@ void encrypt2(char *str){
     }
 }
 
+void decryption1(char *estr){
+    for(int i=0;estr[i]!='\0';i++){
+        if((estr[i]>=65 && estr[i]<=90) || (estr[i]>=97 && estr[i]<=122))
+            estr[i]-=2;   //estr[i]=estr[i]-2;
+
+    }
+}
+
+void decrypt2(char *str){
+    for(int i=0;str[i]!='\0';i++){
+        if((str[i]>=65 && str[i]<=90) || (str[i]>=97 && str[i]<=122) ||  str[i]>=33 && str[i]<=42||str[i]==64){
+         if(i<=5){
+             str[i] -= 5;
+         }
+         else{
+             str[i] -= 7;
+         }
+        }
+    
+}
+}
+
+
 void main(){
     char string[100];
     char choice[50];
