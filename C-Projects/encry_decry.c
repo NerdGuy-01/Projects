f6300e8ee44b3ea6efbeacae29453038d9b73fcd
Dhruv-ver2 <dhruv2vaishnav@gmail.com>
2025-02-12 20:11:01 +0530

    Update encry_decry.c
    Added two encryption functions

--- C-Projects/encry_decry.c
+++ C-Projects/encry_decry.c
@@ -2,6 +2,28 @@
 #include<stdio.h>
 #include<string.h>
 
+//Encryption1: Shifting the alphabetical characters by 2 steps
+void encryption1(char *str){
+    for(int i=0;str[i]!='\0';i++){
+        if((str[i]>=65 && str[i]<=90) || (str[i]>=97 && str[i]<=122))
+            str[i]+=2; //str[i]=str[i]+2;
+
+    }
+
+}
+
+//Encryption2: Shifting the first 5 elements by 5(ASCII) steps and remaining by 7(ASCII) steps
+void encrypt2(char *str){
+    for(int i=0;str[i]!='\0';i++){
+        if((str[i]>=65 && str[i]<=90) || (str[i]>=97 && str[i]<=122) ||  str[i]>=33 && str[i]<=42||str[i]==64){
+         if(i<=5)
+             str[i] += 5;
+         else
+             str[i] += 7;
+        }
+    }
+}
+
 void main(){
     char string[100];
     char choice[50];
