8615d9a96c1bc043566f22fb0d25359336fbb7fa
Dhruv-ver2 <dhruv2vaishnav@gmail.com>
2025-02-12 22:51:11 +0530

    Update encry_decry.c
    Edited the whole code based on Aarish encryption

--- C-Projects/encry_decry.c
+++ C-Projects/encry_decry.c
@@ -52,35 +52,14 @@ void main(){
     char choice[50];
     printf("Enter the string:");
     scanf("%s",string);
-
-    for(int i=0;string[i]!='\0';i++){
-        if((string[i]>=65 && string[i]<=90) || (string[i]>=97 && string[i]<=122) ||  string[i]>=33 && string[i]<=42||string[i]==64){
-         if(i<=5){
-             string[i] += 5;
-         }
-         else {
-             string[i] += 7;
-         }
-        }
-    
-    }
-
-    printf("The encrypted string is:- %s",string);
+    encrypt2(string);
+     printf("The encrypted string is:- %s",string);
   // For decryption
     printf("\nWant to decrypt?(yes or no )");
-scanf("%s",choice);
-if(strcmp(choice,"yes")==0 || strcmp(choice,"Yes")==0){
-    for(int i=0;string[i]!='\0';i++){
-        if((string[i]>=65 && string[i]<=90) || (string[i]>=97 && string[i]<=122) ||  string[i]>=33 && string[i]<=42||string[i]==64){
-         if(i<=5){
-             string[i] -= 5;
-         }
-         else{
-             string[i] -= 7;
-         }
-        }
+    scanf("%s",choice);
+    if(strcmp(choice,"yes")==0 || strcmp(choice,"Yes")==0)
+        decrypt2(string);
+
     
-    }
     printf("Your original string  is : %s",string);
-}
 }
