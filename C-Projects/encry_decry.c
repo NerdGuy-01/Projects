d844cbb89ff8e01cd1bd5b64f632d1686308abed
Aarish mansur <aarishmansur@gmail.com>
2025-02-13 12:00:34 +0530

    Update encry_decry.c

--- C-Projects/encry_decry.c
+++ C-Projects/encry_decry.c
@@ -6,7 +6,7 @@
 void encrypt1(char *str){
     for(int i=0;str[i]!='\0';i++){
         if((str[i]>=65 && str[i]<=90) || (str[i]>=97 && str[i]<=122))
-            str[i]+=2; //str[i]=str[i]+2;
+            str[i]+=2; 
 
     }
 
