63fd74826670aed0505ae8b829c9e7a125bb319a
aarishmansur <aarishmansur@gmail.com>
2025-03-04 17:24:19 +0530

    maintained a random word

--- C-Projects/Hang.c
+++ C-Projects/Hang.c
@@ -3,19 +3,21 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct variables{
-  char choice[3];
-  int random_words;
-}
-
 int main(){
+  char choice[3];
+  char options[][7] = {"Luffy","Saitama","Zorro"};
+
   printf("Hello Welcome to this Hangman game");
   printf("(would you like to know rules ?)");
   scanf("%s",choice);
+
   if(strcmp(choice,"Yes")==0 || strcmp(choice,"yes")){
     printf(" 1. There is given a word with omitted characters and you need to guess the characters to win the game.");
     printf(" 2. If you guessed all characters right the  man lives otherwise he Dies");
   }
   
+ int size = sizeof(options)/sizeof(options[0]); // To get number of elements
+ 
+  int random_index = rand() % size;
 
 }
